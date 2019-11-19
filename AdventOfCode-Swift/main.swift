6b140b19ac415fffebaf8c939758fc5c2bb8c809
Dean151 <thomas.durand.verjat@gmail.com>
2019-11-19 22:31:02 +0100

    Initial Commit

---
+++ AdventOfCode-Swift/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  AdventOfCode-Swift
+//
+//  Created by Thomas Durand on 19/11/2019.
+//  Copyright © 2019 Thomas Durand. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
