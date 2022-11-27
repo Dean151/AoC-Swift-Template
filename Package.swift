51c0819b5d5345145a75c9eab648594bb4d43b40
Thomas Durand <thomas.durand.verjat@gmail.com>
2022-11-27 18:33:57 +0100

    Added swift crypto, that can be of use

--- Package.swift
+++ Package.swift
@@ -39,12 +39,14 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/apple/swift-async-algorithms", from: "0.0.0"),
         .package(url: "https://github.com/apple/swift-collections", from: "1.0.0"),
+        .package(url: "https://github.com/apple/swift-crypto.git", from: "2.0.0"),
     ],
     targets: [
         .target(name: "AoC"),
         .target(name: "Common", dependencies: [
             .product(name: "AsyncAlgorithms", package: "swift-async-algorithms"),
             .product(name: "Collections", package: "swift-collections"),
+            .product(name: "Crypto", package: "swift-crypto"),
         ]),
 
         .executableTarget(name: "Day01", dependencies: ["AoC", "Common"], resources: [.process("input.txt")]),
