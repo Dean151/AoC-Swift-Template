bcba4821cf90990c23d9a0fd833c4709dc2f783a
Thomas Durand <thomas.durand.verjat@gmail.com>
2025-01-02 23:18:26 +0100

    swift-algorithms instead of swift-async-algorithms

--- Package.swift
+++ Package.swift
@@ -37,14 +37,14 @@ let package = Package(
         .executable(name: "Day25", targets: ["Day25"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-async-algorithms", from: "0.0.0"),
+        .package(url: "https://github.com/apple/swift-algorithms", from: "1.2.0"),
         .package(url: "https://github.com/apple/swift-collections", from: "1.0.0"),
         .package(url: "https://github.com/apple/swift-crypto.git", from: "2.0.0"),
     ],
     targets: [
         .target(name: "AoC"),
         .target(name: "Common", dependencies: [
-            .product(name: "AsyncAlgorithms", package: "swift-async-algorithms"),
+            .product(name: "Algorithms", package: "swift-algorithms"),
             .product(name: "Collections", package: "swift-collections"),
             .product(name: "Crypto", package: "swift-crypto"),
         ]),
